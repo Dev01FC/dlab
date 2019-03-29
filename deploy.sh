f79826b00ac9377e12d530b42d204724984f6c38
frankleafy <leafy@nospoon.nl>
2019-03-29 11:58:49 +0100

    h

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
  #git push -f git@github.com:frankleafy/dl58.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Dev01FC/FCDLab.git master:gh-pages
+ git push -f git@github.com:Dev01FC/dlab.git master:gh-pages
 
 cd -
